a60b2bd297655fb95d0d4f723d189fd9c0d1fcc6
rfaldetta <ross.faldetta@gmail.com>
2015-08-23 23:00:20 -0500

    adds user associations

--- app/models/practice_interview.rb
+++ app/models/practice_interview.rb
@@ -4,4 +4,5 @@ class PracticeInterview < ActiveRecord::Base
 
   has_many :practice_questions , :class_name => "PracticeQuestion", :foreign_key => "practice_interview_id"
   has_many :questions, :through => :practice_questions
+  belongs_to :user
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -8,4 +8,5 @@ class Question < ActiveRecord::Base
   has_many :practice_interviews , :class_name => "PracticeQuestion", :foreign_key => "question_id"
   belongs_to :category , :class_name => "Category", :foreign_key => "category_id"
   belongs_to :question_length
+  belongs_to :user , :class_name => "User", :foreign_key => "user_id"
 end
